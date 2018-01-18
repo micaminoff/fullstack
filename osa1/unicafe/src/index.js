01f9a46f29c6a388239d97c1d7a339728c3cc30c
Michael Aminoff <michael@noviaria.com>
2018-01-18 23:10:16 +0200

    1.6

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hyva: 0,
+            neutraali: 0,
+            huono: 0
+        }
+    }
+
+    klikHyva = () => {
+        this.setState({
+            hyva: this.state.hyva + 1,
+        })
+    }
+    klikNeutraali = () => {
+        this.setState({
+            neutraali: this.state.neutraali + 1,
+        })
+    }
+    klikHuono = () => {
+        this.setState({
+            huono: this.state.huono + 1,
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Anna Palautetta</h1>
+                <button onClick={this.klikHyva}>Hyvä!</button>
+                <button onClick={this.klikNeutraali}>Neutraali</button>
+                <button onClick={this.klikHuono}>huono...</button>
+                
+                <h1>Statistiikka</h1>
+                <p>Hyvä: {this.state.hyva}</p>
+                <p>Neutraali: {this.state.neutraali}</p>
+                <p>Huono: {this.state.huono}</p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
