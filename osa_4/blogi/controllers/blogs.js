fe47f16e6d932f3db7f44e72c7e2bfe750e5f852
Michael <micaminoff@gmail.com>
2018-02-24 16:26:19 +0200

    4.2

---
+++ osa_4/blogi/controllers/blogs.js
@@ -0,0 +1,32 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+const formatBlog = (blog) => {
+    return {
+        id: blog._id,
+        title: blog.title,
+        author: blog.author,
+        url: blog.url,
+        likes: blog.likes
+    }
+}
+
+blogRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs.map(formatBlog))
+    })
+})
+
+blogRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogRouter
