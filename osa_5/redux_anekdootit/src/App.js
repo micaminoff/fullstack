0e1931b1d4fd9adbc4d320c40152c2891d6c1f79
Michael Aminoff <michael@noviaria.com>
2018-03-04 20:29:37 +0200

    5.19

---
+++ osa_5/redux_anekdootit/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    const anecdotes = this.props.store.getState()
+    return (
+      <div>
+        <h2>Anecdotes</h2>
+        {anecdotes.map(anecdote=>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content} 
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => this.props.store.dispatch({ type: 'VOTE', id: anecdote.id })}>vote</button>
+            </div>
+          </div>
+        )}
+        <h2>create new</h2>
+        <form>
+          <div><input /></div>
+          <button>create</button> 
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
